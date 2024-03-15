af177bc7f233e35acedbc2157d8852b04cbc5c6b
ehpark <silbroeh@gmail.com>
2024-03-15 13:53:30 +0900

    refactor : ParameterUtils 클래스 리팩토링
    상수 분리 및 테스트코드 수정

--- src/main/java/utils/ParameterUtils.java
+++ src/main/java/utils/ParameterUtils.java
@@ -5,16 +5,16 @@ import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ParameterParser {
+public class ParameterUtils {
+    private static final String PARAM_DELIMITER = "&";
+    private static final String KEY_VALUE_DELIMITER = "=";
 
-    public static Map<String, String> getUserParams(String input) {
+    public static Map<String, String> parseUserParams(String userInfo) {
         Map<String, String> params = new HashMap<>();
-        for (String param : input.split("&")) {
-            String[] keyValue = param.split("=");
+        for (String param : userInfo.split(PARAM_DELIMITER)) {
+            String[] keyValue = param.split(KEY_VALUE_DELIMITER);
             try {
-                String key = keyValue[0];
-                String value = URLDecoder.decode(keyValue[1], "UTF-8");
-                params.put(key, value);
+                params.put(keyValue[0], URLDecoder.decode(keyValue[1], "UTF-8"));
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
--- src/test/java/utils/ParameterUtilsTest.java
+++ src/test/java/utils/ParameterUtilsTest.java
@@ -4,19 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-class ParameterParserTest {
+class ParameterUtilsTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+        "userId, javajigi",
+        "password, password",
+        "name, 박재성",
+        "email, javajigi@slipp.net"
+    })
     @DisplayName("사용자 정보를 담은 문자열 파싱 후 parameter 추출 기능 검증")
-    void getUserInfo() {
-        String input = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
-        Map<String, String> userInfo = ParameterParser.getUserParams(input);
+    void parseUserParamsTest(String input, String expectedOutput) {
+        String userString = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+        Map<String, String> userParams = ParameterUtils.parseUserParams(userString);
 
-        assertThat(userInfo.get("userId")).isEqualTo("javajigi");
-        assertThat(userInfo.get("password")).isEqualTo("password");
-        assertThat(userInfo.get("name")).isEqualTo("%EB%B0%95%EC%9E%AC%EC%84%B1");
-        assertThat(userInfo.get("email")).isEqualTo("javajigi%40slipp.net");
+        assertThat(userParams.get(input)).isEqualTo(expectedOutput);
     }
 }
