e58493996026f308f659e81d84646e906ea845bc
ehpark <silbroeh@gmail.com>
2024-03-20 00:21:18 +0900

    refactor : http 프로세스 참고 후 필드 및 메소드 이름 변경

--- src/main/java/utils/ParameterUtils.java
+++ src/main/java/utils/ParameterUtils.java
@@ -9,14 +9,14 @@ public class ParameterUtils {
     private static final String PARAM_DELIMITER = "&";
     private static final String KEY_VALUE_DELIMITER = "=";
 
-    public static Map<String, String> parseParams(String userInfo) {
+    public static Map<String, String> parseParams(String query) {
         Map<String, String> params = new HashMap<>();
-        for (String param : userInfo.split(PARAM_DELIMITER)) {
+        for (String param : query.split(PARAM_DELIMITER)) {
             String[] keyValue = param.split(KEY_VALUE_DELIMITER);
             try {
                 params.put(keyValue[0], URLDecoder.decode(keyValue[1], "UTF-8"));
             } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
+                throw new IllegalArgumentException();
             }
         }
         return params;
